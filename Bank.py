35822181ef351252c95126b31b327771d17cc75e
Tumelo Edward Ntsilo <122849519+TumeloNtsilo@users.noreply.github.com>
2025-02-12 13:59:45 +0200

    Add files via upload

---
+++ Bank.py
@@ -0,0 +1,65 @@
+from client import Client
+import random
+
+
+bank_name = "T.EN Bank"
+bank_clients = []
+
+def greetings():
+    print(f"Welcome to {bank_name}.")
+    print()
+
+client = Client()
+#Create a bank account
+choices = ["Open a bank account", "Use an existing bank account"]
+for index, choice in enumerate(choices):
+    print(f"{index}: {choice}")
+print()
+
+while True:
+    try:
+        banking_options = int(input(f"How can we help you: {list(enumerate(choices))}: "))
+        if 0 <= banking_options < len(choices):
+            break
+        else:
+            print("Invalid input, try again.")
+    except ValueError:
+        print("Invalid input, please enter a number")
+    
+user_choice = choices[banking_options]
+print()
+
+client_details = {}
+
+if user_choice == choices[0]:
+    print("Please fill your information below")
+    while True:
+        try:
+            name = input("Enter your full name: ")
+            identity_num = int(input("Enter your ID number: "))
+            if len(str(identity_num)) != 13:
+                print("Invalid ID number.")
+                break
+            sex = input("Enter your gender: ")
+        except:
+            print("Invalid inputs")
+        pin_num = random.randint(1000, 9999)
+        print(f"Here is your account number: {client.account_num}")
+        print(f"Here is your pin number, keep it safe and a secret: {pin_num}")
+        print()
+
+        client_details["name"] = name
+        client_details["account number"] = client.account_num
+        bank_clients.append(client_details)
+        break
+
+elif user_choice == choices[1]:
+    client.enter_pin()
+    client.display_account_details()
+
+    while True:
+        client.action()
+        if client.chosen_option == "quit.":
+            print("Thank you for banking with us.")
+            break
+        client.bank(client.chosen_option)
---
+++ client.py
@@ -0,0 +1,93 @@
+import random
+
+class Client:
+    
+    def __init__(self):
+        self.account_type = "savings"
+        self.account_num = random.randint(10000000, 99999999)
+        self.balance = 0.00
+
+    def enter_pin(self):
+        while True:
+            try:
+                self.pin = int(input("To access your account, enter four digit pin: "))
+                if len(str(self.pin)) != 4:
+                    print("Invalid pin.")
+                else:
+                    break
+
+            except:
+                print("Invalid pin.")
+    
+    def display_account_details(self):
+        print(f"Account number: {self.account_num}\nAccount type: {self.account_type}\n")
+    
+    def deposit(self):
+        self.amount = float(input("Enter the amount to deposit: "))
+        if 10 <= self.amount < 100:
+            self.charges = 1.50
+
+        elif 100 <= self.amount < 1000:
+            self.charges = 2.5
+
+        elif self.charges > 1000:
+            self.charges = 7.5
+
+        self.balance += self.amount - self.charges
+
+        print(f"balance: {self.balance}")
+        print()
+
+    def withdraw(self):
+        self.amount = float(input("Enter the amount you want to withdraw: "))
+        if self.amount > self.balance:
+            print("Insuffient funds!")
+
+        elif 10 <= self.amount < 100:
+            self.charges = 1.0
+
+        elif 100 <= self.amount < 1000:
+            self.charges = 2.00
+
+        elif self.charges > 1000:
+            self.charges = 8.00
+            
+            self.balance -= self.amount - self.charges
+
+        print(f"Balance: {self.balance}")
+        print()
+
+    def action(self):
+        self.options = ["view balance.", "withdraw.", "deposit", "quit."]
+        for index, option in enumerate(self.options):
+            print(f"{index}: {option}")
+        
+        print()
+
+        while True:
+            try:
+                self.choose_option = int(input(f"How can we help you? {list(enumerate(self.options))}: "))
+                if 0 <= self.choose_option < len(self.options):
+                    break
+                else:
+                    print("Invalid choice, try again.")
+            except ValueError:
+                print("Invalid option, enter a number.")
+
+        self.chosen_option = self.options[self.choose_option]    
+
+    def bank(self, option):
+        if option == "view balance.":
+            print(f"Balance: {self.balance}")
+            print()
+
+        elif option == "withdraw.":
+            self.withdraw()
+
+        elif option == "deposit":
+            self.deposit()
+        
+
+
+
+    
