bc616972e00934b9f113c27cb969c96b9ac6f87d
Tumelo Edward Ntsilo <122849519+TumeloNtsilo@users.noreply.github.com>
2025-02-14 10:39:07 +0200

    Update client.py

--- client.py
+++ client.py
@@ -8,12 +8,13 @@ class Client:
         self.balance = 0.00
 
     def enter_pin(self):
-        attempts = 3
+        attempts  = 3
+
         while attempts > 0:
             try:
                 self.pin = int(input("To access your account, enter four digit pin: "))
                 if len(str(self.pin)) != 4:
-                    print("Invalid pin.")
+                    print(f"Invalid pin, {attempts -1} remaining.")
                 else:
                     break
             except:
@@ -33,10 +34,10 @@ class Client:
         elif self.amount > 1000:
             self.charges = 7.5
 
-        self.balance += self.amount - self.charges
+        self.balance += (self.amount - self.charges)
 
-        print(f"balance: {self.balance}")
-        print()
+        print(f"balance: {self.balance}\n")
+    
 
     def withdraw(self):
         self.amount = float(input("Enter the amount you want to withdraw: "))
@@ -45,46 +46,41 @@ class Client:
 
         else:
             if 10 <= self.amount < 100:
-            self.charges = 1.00
+                self.charges = 1.0
 
-        elif 100 <= self.amount < 1000:
-            self.charges = 2.00
+            elif 100 <= self.amount < 1000:
+                self.charges = 2.00
 
-        elif self.amount > 1000:
-            self.charges = 8.00
-            
-        else: 
-            self.charges = 0.00
+            elif self.amount > 1000:
+                self.charges = 8.00
+                
+                self.balance -= (self.amount - self.charges)
 
-        self.balance -= (self.amount + self.charges)
-            
-        print(f"Balance: {self.balance}")
-        print()
+        print(f"Balance: {self.balance}\n")
+
 
     def action(self):
         self.options = ["view balance", "withdraw", "deposit", "quit"]
         for index, option in enumerate(self.options):
-            print(f"{index}: {option}")
-        
-        print()
+            print(f"{index}: {option}\n")
 
         while True:
             try:
                 self.choose_option = int(input(f"How can we help you? {list(enumerate(self.options))}: "))
-                if 0 <= self.choose_index < len(self.options):
-                    self.chosen_index = self.options[self.choose_option]
+                if 0 <= self.choose_option < len(self.options):
+                    self.chosen_index = self.options[self.choose_option]  
                     break
                 else:
                     print("Invalid choice, try again.")
             except ValueError:
                 print("Invalid option, enter a number.")
 
-    def bank(self, option):
-        if option == "view balance.":
-            print(f"Balance: {self.balance}")
-            print()
 
-        elif option == "withdraw.":
+    def bank(self, option):
+        if option == "view balance":
+            print(f"Balance: {self.balance}\n")
+
+        elif option == "withdraw":
             self.withdraw()
 
         elif option == "deposit":
