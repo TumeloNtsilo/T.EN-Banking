60cc3f84172964ecdf7d98bb6afc6fb2a77cdb55
Tumelo Edward Ntsilo <122849519+TumeloNtsilo@users.noreply.github.com>
2025-02-18 23:34:46 +0200

    Update client.py

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ class Client:
     def __init__(self):
         self.account_type = "savings"
         self.account_num = random.randint(10000000, 99999999)
+        self.pin_number = random.randint(1000, 9999)
         self.balance = 0.00
 
     def enter_pin(self):
@@ -12,13 +13,22 @@ class Client:
 
         while attempts > 0:
             try:
-                self.pin = int(input("To access your account, enter four digit pin: "))
-                if len(str(self.pin)) != 4:
-                    print(f"Invalid pin, {attempts -1} remaining.")
+                self.entered_pin = int(input("To access your account, enter four digit pin: "))
+                if len(str(self.pin)) == 4 and self.entered_pin.isdigit():
+                    if int(self.entered_pin) == self.pin_number:
+                        print(f"Pin is corret.")
+                    else:
+                        attempts -= 1
+                        print(f"\nInvalid pin, please try again.")
                 else:
-                    break
+                    attempts -= 1
+                    print(f"\nInvalid pin, please try again.")
+                
             except:
                 print("Invalid pin.")
+                attempts -= 1
+        print(f"\n Finished all attempts.")
+        exit()
     
     def display_account_details(self):
         print(f"Account number: {self.account_num}\nAccount type: {self.account_type}\n")
@@ -43,6 +53,7 @@ class Client:
         self.amount = float(input("Enter the amount you want to withdraw: "))
         if self.amount > self.balance:
             print("Insuffient funds!")
+            return 
 
         else:
             if 10 <= self.amount < 100:
@@ -85,7 +96,6 @@ class Client:
 
         elif option == "deposit":
             self.deposit()
-        
 
 
 
