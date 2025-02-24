b387aeadf12889bd1be5daa1a8052f404701860c
Tumelo Edward Ntsilo <122849519+TumeloNtsilo@users.noreply.github.com>
2025-02-24 12:20:19 +0200

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -5,47 +5,48 @@ import random
 def greetings():
     print(f"Good day and welcome to {bank_name}.\n")
 
+
 def new_account():
     print(f"Let us open a new account for you.\n")
     client_details = {} 
     print("Please fill in your information below")
+
     while True:
         try:
             name = input("Enter your full name: ")
             identity_num = int(input("Enter your ID number: "))
             address = input("Enter address in one line with spaces in between: ")
-            tittle = input("Enter your tittle Mr, Miss, Ms, Mrs, or other: ")
-            if len(str(identity_num)) != 13 or not identity_num.isdigit():
+            if len(str(identity_num)) != 13:
                 print("Invalid ID number.")
-                break   
+                break
+            tittle = input("Enter your tittle Mr, Miss, Ms, Mrs, or other: ")
         except:
             print("Invalid inputs")
-        print()
 
-        print(f"{tittle} {name}\n{identity_num}")
+        print(f"\n{tittle} {name}\n{identity_num}")
         for x in address.split(" "):
             print(x)
         confirmation = input(f"Are your details correct (yes/no)?: ").lower()
         if confirmation == "yes":
-            global client
-            client = Client
-
             print(f"\nHere is your account number: {client.account_num}")
             print(f"Here is your pin number, keep it safe and a secret: {client.pin_number}\n")
-            client_details["name"] = f"{tittle} {name}"
-            client_details["account number"] = client.account_num
-            client_details["pin number"] = client.pin_number
-            bank_clients.append(client_details)
 
         elif confirmation == "no":
-            print("Please start over!")
+            print(f"Please start over!\n")
             new_account()
         
+
+        client_details["name"] = f"{tittle} {name}"
+        client_details["account number"] = client.account_num
+        client_details["pin number"] = client.pin_number
+        bank_clients.append(client_details)
         break
+
 
 def use_account():
     print(f"Now, you can use your account")
     client.enter_pin()
+    client.display_account_details()
 
     while True:
         client.action()
@@ -63,7 +64,7 @@ if __name__ == "__main__":
     greetings()
     new_account()
     use_account()
-    client = Client()
+    
 
     greetings()
     new_account()
